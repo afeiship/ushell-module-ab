69a91e94caf8b979224af62861467bd388f52ae6
afeiship <1290657123@qq.com>
2020-11-24 10:09:09 +0800

    fix: configure: error: APR not found.  Please read the documentation.

--- index.sh
+++ index.sh
@@ -2,7 +2,10 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+# apr
 echo 'export PATH="/usr/local/opt/apr/bin:$PATH"' >> ~/.profile
+
+# apr-util
 echo 'export PATH="/usr/local/opt/apr-util/bin:$PATH"' >> ~/.profile
 
 unset ROOT_PATH;
--- initialize.sh
+++ initialize.sh
@@ -3,6 +3,7 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 # https://zhuanlan.zhihu.com/p/59918109
+# https://blog.phpha.com/archives/142/
 
 # 1. install deps
 brew install apr 
@@ -15,9 +16,10 @@ echo 'export PATH="/usr/local/opt/apr/bin:$PATH"' >> ~/.profile
 # 3. install 5 steps
 wget https://mirror-hk.koddos.net/apache//httpd/httpd-2.4.46.tar.gz
 cd httpd-2.4.46
-./configure
+
+# 下面两个路径，根据情况修改
+./configure --with-apr=/usr/local/Cellar/apr/1.7.0/ --with-apr-util=/usr/local/Cellar/apr-util/1.6.1_3/
 make
 make install
-
 
 unset ROOT_PATH;
